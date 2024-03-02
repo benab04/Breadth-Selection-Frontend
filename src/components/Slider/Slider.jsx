1c2e838a8fa65a326ddb792af33f550a31131ff7
benab04 <benabrahambiju@gmail.com>
2024-03-02 13:35:33 +0530

    update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,21 +1,16 @@
 import React, { useState } from "react";
-
+import "./Slider.css";
+import { options } from "../../assets/data/dropdown";
 const SliderComponent = () => {
   const [selectedPreferences, setSelectedPreferences] = useState([]);
   const [preferenceValues, setPreferenceValues] = useState({});
   const [usePreferences, setUsePreferences] = useState(false);
   const [response, setResponse] = useState(null);
+  const [typedPreference, setTypedPreference] = useState("");
 
   const handlePreferenceChange = (e) => {
     const selectedPref = e.target.value;
-    if (!selectedPreferences.includes(selectedPref)) {
-      const updatedPrefs = [...selectedPreferences, selectedPref];
-      setSelectedPreferences(updatedPrefs);
-      setPreferenceValues((prevState) => ({
-        ...prevState,
-        [selectedPref]: 50, // Set default value to 50%
-      }));
-    }
+    addPreference(selectedPref);
   };
 
   const handleSliderChange = (e, preference) => {
@@ -24,12 +19,23 @@ const SliderComponent = () => {
     setPreferenceValues(updatedPrefValues);
   };
 
+  const handleRemovePreference = (preference) => {
+    setSelectedPreferences(
+      selectedPreferences.filter((pref) => pref !== preference)
+    );
+    setPreferenceValues((prevState) => {
+      const updatedValues = { ...prevState };
+      delete updatedValues[preference];
+      return updatedValues;
+    });
+  };
+
   const handleSubmit = () => {
     const formData = new FormData();
     selectedPreferences.forEach((pref) => {
       formData.append(
         pref,
-        usePreferences ? preferenceValues[pref] / 100 : 0.5
+        usePreferences ? preferenceValues[pref] / 100 : 1.0
       );
     });
 
@@ -47,26 +53,111 @@ const SliderComponent = () => {
       });
   };
 
-  return (
-    <div className="container">
-      <div className="mb-3">
-        <label htmlFor="preferenceSelect" className="form-label">
-          Select Preferences:
-        </label>
-        <select
-          className="form-select"
-          id="preferenceSelect"
-          onChange={handlePreferenceChange}
-        >
-          <option value="">Select...</option>
-          <option value="Aerodynamics">Aerodynamics</option>
-          <option value="Mathematics">Mathematics</option>
-          <option value="Language">Language</option>
-        </select>
+  const addPreference = (preference) => {
+    if (preference && !selectedPreferences.includes(preference)) {
+      setSelectedPreferences([...selectedPreferences, preference]);
+      setPreferenceValues((prevState) => ({
+        ...prevState,
+        [preference]: 100, // Set default value to 100%
+      }));
+      setTypedPreference("");
+    }
+  };
+
+  const handleTypedPreferenceSubmit = (e) => {
+    e.preventDefault();
+    addPreference(typedPreference);
+  };
+
+  const renderSliders = () => {
+    const sliders = selectedPreferences.map((pref, index) => (
+      <div className="col-12 col-sm-6 col-md-4 mb-3" key={pref}>
+        <div className="mb-3">
+          <label htmlFor={`preferenceSlider-${pref}`} className="form-label">
+            {pref}
+          </label>
+          <input
+            type="range"
+            className="form-range"
+            id={`preferenceSlider-${pref}`}
+            min="0"
+            max="100"
+            value={preferenceValues[pref]}
+            onChange={(e) => handleSliderChange(e, pref)}
+          />
+          <p>{preferenceValues[pref]}%</p>
+        </div>
       </div>
+    ));
+
+    const rows = [];
+    for (let i = 0; i < sliders.length; i += 3) {
+      rows.push(
+        <div className="row" key={i}>
+          {sliders.slice(i, i + 3)}
+        </div>
+      );
+    }
+    return rows;
+  };
+
+  return (
+    <div className="container my-5 ">
+      <div className="row mb-3">
+        <div className="col">
+          <label htmlFor="preferenceSelect" className="form-label">
+            Select Preferences:
+          </label>
+          <select
+            className="form-select"
+            id="preferenceSelect"
+            onChange={handlePreferenceChange}
+          >
+            <option value="">Select...</option>
+            {options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="col">
+          <form onSubmit={handleTypedPreferenceSubmit}>
+            <label htmlFor="typedPreferenceInput" className="form-label">
+              Additional Preferences:
+            </label>
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                id="typedPreferenceInput"
+                value={typedPreference}
+                onChange={(e) => setTypedPreference(e.target.value)}
+              />
+              <button type="submit" className="btn btn-primary">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+
       {selectedPreferences.length > 0 && (
         <div className="mb-3">
-          <p>Selected Preferences: {selectedPreferences.join(", ")}</p>
+          <p>Selected Preferences:</p>
+          <div className="d-flex ">
+            {selectedPreferences.map((pref) => (
+              <div className="preference-box" key={pref}>
+                {pref}
+                <button
+                  className="close-btn"
+                  onClick={() => handleRemovePreference(pref)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+          </div>
           <div className="form-check">
             <input
               className="form-check-input"
@@ -84,24 +175,12 @@ const SliderComponent = () => {
           </div>
         </div>
       )}
-      {usePreferences &&
-        selectedPreferences.map((pref) => (
-          <div className="mb-3" key={pref}>
-            <label htmlFor={`preferenceSlider-${pref}`} className="form-label">
-              {pref}
-            </label>
-            <input
-              type="range"
-              className="form-range"
-              id={`preferenceSlider-${pref}`}
-              min="0"
-              max="100"
-              value={preferenceValues[pref]}
-              onChange={(e) => handleSliderChange(e, pref)}
-            />
-            <p>{preferenceValues[pref]}%</p>
-          </div>
-        ))}
+      {usePreferences && (
+        <div className="mb-3">
+          <p>Selected Preferences:</p>
+          {renderSliders()}
+        </div>
+      )}
       <button
         className="btn btn-primary"
         onClick={handleSubmit}
@@ -109,10 +188,34 @@ const SliderComponent = () => {
       >
         Submit
       </button>
+
       {response && (
         <div className="mt-3">
           <p>Response from server:</p>
-          <pre>{JSON.stringify(response, null, 2)}</pre>
+          <pre>
+            {" "}
+            <div className="container">
+              <h2>Course Details</h2>
+              <table className="table table-striped">
+                <thead>
+                  <tr>
+                    <th>Course</th>
+                    <th>Final Score</th>
+                    <th>Search Score</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {response.map((course, index) => (
+                    <tr key={index}>
+                      <td>{course.course}</td>
+                      <td>{course.final_score}</td>
+                      <td>{course.search_score}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </pre>
         </div>
       )}
     </div>
